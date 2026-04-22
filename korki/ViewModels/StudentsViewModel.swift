88fccaf8c30386eb9fe82b05ae88cff21f0b7905
skarbus67 <wroclawska148@gmail.com>
2026-04-22 16:04:20 +0200

    chore: clean up and add View, Models and ViewModels groups

---
+++ korki/ViewModels/StudentsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  StudentsViewModel.swift
+//  korki
+//
+//  Created by Mikołaj Skarbek on 22/04/2026.
+//
+
+import Foundation
---
+++ korki/Views/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  korki
+//
+//  Created by Mikołaj Skarbek on 22/04/2026.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
