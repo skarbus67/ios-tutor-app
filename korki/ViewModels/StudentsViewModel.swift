c3aa4a9f988332499ef8604143a61f1a60f5f8e5
skarbus67 <wroclawska148@gmail.com>
2026-04-26 16:09:32 +0200

    feat: add StudentsViewModel with addition and deletion functionality

--- korki/ViewModels/StudentsViewModel.swift
+++ korki/ViewModels/StudentsViewModel.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import SwiftData
+import Observation
+
+@Observable
+class StudentsViewModel {
+    func addStudent(name: String, surname: String, hourlyRate: Int, subject: String, context: ModelContext){
+        context.insert(Student(name: name, surname: surname, hourlyRate: hourlyRate, subject: subject))
+        try? context.save()
+    }
+    
+    
+    func deleteStudent(student: Student, context: ModelContext){
+        context.delete(student)
+        try? context.save()
+    }
+    
+}
