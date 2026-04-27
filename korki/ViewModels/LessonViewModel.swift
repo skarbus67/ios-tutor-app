6812f57674dd0822bff04194355d26a5a2331054
skarbus67 <wroclawska148@gmail.com>
2026-04-27 14:53:43 +0200

    feat: add LessonViewModel with lesson addition functionality

---
+++ korki/ViewModels/LessonViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  LessonViewModel.swift
+//  korki
+//
+//  Created by Mikołaj Skarbek on 27/04/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Observable
+class LessonViewModel{
+    var lessonStartDate: Date = .now
+    var lessonEndDate: Date = .now.advanced(by: 3600)
+    var paymentDate: Date = .now
+    var paymentAmount: Int? = nil
+    var lessonDescription: String = ""
+    var isPaid: Bool = false
+    
+    
+    func addLessonToStudent(student: Student, context: ModelContext){
+        let newLesson = Lesson(
+            lessonStartDate: lessonStartDate,
+            lessonEndDate: lessonEndDate,
+            paymentDate: paymentDate,
+            paymentAmount: paymentAmount!,
+            lessonDescription: lessonDescription,
+            isPaid: isPaid)
+        
+        newLesson.student = student
+        
+        context.insert(newLesson)
+    }
+    
+    
+    
+    
+    
+}
